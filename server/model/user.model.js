a1ac89665e41bc77fb62fb0ffe1ac5e70c834e3a
Ashitosh Bhise <132629679+Ashitosh-Bhise@users.noreply.github.com>
2024-02-25 11:46:12 +0530

    Update user.model.js

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 import crypto from "crypto";
+
 const userSchema = new Schema(
   {
     fullname: {
